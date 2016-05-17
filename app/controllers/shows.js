f9c4a6025a1201037dfe30320a02b3e142ad5eb3
nathan <njgingrich@gmail.com>
2016-05-17 16:11:18 -0400

    moved create show into modal

--- app/controllers/shows.js
+++ app/controllers/shows.js
@@ -1,12 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  openModal: false,
+
   actions: {
+    openAddShowModal() {
+      this.set('openModal', true);
+    },
     createShow(name) {
+      console.log(name);
       let show1 = this.store.createRecord('show', {
         name: name
       });
       show1.save();
+      this.set('openModal', false);
     },
     addChar(show, name) {
       let char1 = this.store.createRecord('character', {

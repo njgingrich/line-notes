9446ec589505433418b7f44a32d649a924d4072c
nathan <njgingrich@gmail.com>
2016-05-17 16:14:12 -0400

    moved delete show from shows to show-detail controller

--- app/controllers/shows.js
+++ app/controllers/shows.js
@@ -22,23 +22,6 @@ export default Ember.Controller.extend({
       char1.save();
       show.get('characters').pushObject(char1);
       show.save();
-    },
-    deleteShow(show) {
-      console.log('deleting show ' + show.get('name'));
-      // delete its characters
-      show.get('characters').forEach((character) => {
-        Ember.run.once(this, () => {
-          console.log('char: ' + character);
-          character.deleteRecord();
-          character.save();
-        });
-      });
-
-      // TODO: delete each character's line notes
-
-      show.deleteRecord();
-      show.save();
-      this.transitionToRoute('shows');
     }
   }
 });
--- app/controllers/shows/show-detail.js
+++ app/controllers/shows/show-detail.js
@@ -30,6 +30,23 @@ export default Ember.Controller.extend({
       note.deleteRecord();
       note.save();
       char.save();
+    },
+    deleteShow(show) {
+      console.log('deleting show ' + show.get('name'));
+      // delete its characters
+      show.get('characters').forEach((character) => {
+        Ember.run.once(this, () => {
+          console.log('char: ' + character);
+          character.deleteRecord();
+          character.save();
+        });
+      });
+
+      // TODO: delete each character's line notes
+
+      show.deleteRecord();
+      show.save();
+      this.transitionToRoute('shows');
     }
   }
 });

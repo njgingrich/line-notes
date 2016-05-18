7439181291a8b1294d2375e54ca77782699a11bd
nathan <njgingrich@gmail.com>
2016-05-18 15:12:29 -0400

    edit/save button changes color on status change

--- app/components/note-detail.js
+++ app/components/note-detail.js
@@ -3,6 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'tr',
   isEditing: false,
+  buttonType: Ember.computed('isEditing', function() {
+    if (this.get('isEditing')) {
+      return 'btn btn-success';
+    } else {
+      return 'btn btn-warning';
+    }
+  }),
 
   actions: {
     editNote() {

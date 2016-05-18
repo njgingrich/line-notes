5515d423ce0481e360a000d67b298cc220faf9b7
nathan <njgingrich@gmail.com>
2016-05-18 18:50:15 -0400

    moved edit button into own component

--- app/components/char-detail.js
+++ app/components/char-detail.js
@@ -1,19 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isEditing: false,
-  buttonType: Ember.computed('isEditing', function() {
-    if (this.get('isEditing')) {
-      return 'btn btn-success';
-    } else {
-      return 'btn btn-warning';
-    }
-  }),
-
-  actions: {
-    editChar() {
-      this.toggleProperty('isEditing');
-      this.attrs.editChar();
-    }
-  }
+  isEditing: false
 });
--- app/components/note-detail.js
+++ app/components/note-detail.js
@@ -3,17 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'tr',
   isEditing: false,
-  buttonType: Ember.computed('isEditing', function() {
-    if (this.get('isEditing')) {
-      return 'btn btn-success';
-    } else {
-      return 'btn btn-warning';
-    }
-  }),
 
   actions: {
     editNote() {
-      this.toggleProperty('isEditing');
       this.attrs.edit(this.get('note'));
     }
   }

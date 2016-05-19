95c0ec856fcec39ad0b41ca0b71f5cea09b74e98
nathan <njgingrich@gmail.com>
2016-05-19 11:12:56 -0400

    fixed some jsLint errors

--- app/validators/substring.js
+++ app/validators/substring.js
@@ -1,9 +1,8 @@
 import BaseValidator from 'ember-cp-validations/validators/base';
-import Ember from 'ember';
 
 export default BaseValidator.extend({
-  validate(value, options, model, attribute) {
-    if (value == undefined || value === '') {
+  validate(value, options, model/*, attribute*/) {
+    if (value === undefined || value === '') {
       return true;
     }
     if (model.line) {

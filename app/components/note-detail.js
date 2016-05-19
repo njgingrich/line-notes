8430c70beb662cdac6dc51e8d221c01a3cf644e0
nathan <njgingrich@gmail.com>
2016-05-18 22:42:58 -0400

    table row turns warning color when editing

--- app/components/note-detail.js
+++ app/components/note-detail.js
@@ -2,12 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'tr',
+  classNameBindings: ['colorTable'],
   isEditing: false,
   richTextLine: Ember.computed('note.line', 'note.note', function() {
     let boldText = `<b>${this.get('note.note')}</b>`;
     if (this.get('note.line')) {
       let newText = this.get('note.line').replace(this.get('note.note'), boldText);
       return newText;
+    }
+    return '';
+  }),
+  colorTable: Ember.computed('isEditing', function() {
+    if (this.get('isEditing')) {
+      return 'warning';
     }
     return '';
   }),

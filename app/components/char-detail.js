a6e40b97aac5aac1f535de97d95668636d0f3b5e
nathan <njgingrich@gmail.com>
2016-05-23 19:04:58 -0400

    began moving add note into modal + FAB

--- app/components/char-detail.js
+++ app/components/char-detail.js
@@ -2,8 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isEditing: false,
+  openModal: false,
 
   actions: {
+    openAddNoteModal() {
+      this.set('openModal', true);
+    },
+    resetModal() {
+      this.set('name', '');
+      this.set('openModal', false);
+    },
     editChar() {
       console.log("key press submit");
       this.toggleProperty('isEditing');

9265c9035deda8c92b0316fe2d08e02606256bdf
nathan <njgingrich@gmail.com>
2016-05-27 15:30:51 -0400

    line notes sorted by page number

--- app/components/notes-list.js
+++ app/components/notes-list.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    this.sortedNotes = undefined;
+  },
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('sortedNotes', this.get('notes').sortBy('page'));
+  },
   tagName: 'table'
 });

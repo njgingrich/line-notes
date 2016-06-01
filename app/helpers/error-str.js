8b32cdf0bcfc012a193b885766624951490f579b
nathan <njgingrich@gmail.com>
2016-06-01 14:06:07 -0400

    removed console debug

--- app/helpers/error-str.js
+++ app/helpers/error-str.js
@@ -4,7 +4,6 @@ export default Ember.Helper.extend({
   errorTypes: Ember.inject.service('error-types'),
   compute(params) {
     let err = params[0];
-    console.log("capitalizing " + params[0]);
     let str = this.get('errorTypes.errors').objectAt(params[0]);
     if (str) {
       return this.get('errorTypes.errors').objectAt(params[0]).capitalize();

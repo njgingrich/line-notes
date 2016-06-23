889532aa41842604a7e295c8dc7c15fa0e322c4f
nathan <njgingrich@gmail.com>
2016-06-23 01:49:34 -0400

    added sidebar to begin user integration

--- app/controllers/shows.js
+++ app/controllers/shows.js
@@ -1,17 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  openModal: false,
-  newShowName: '',
+  sideBarOpen: false,
 
   actions: {
-    addChar(show, name) {
-      let char1 = this.store.createRecord('character', {
-        name: name
-      });
-      char1.save();
-      show.get('characters').pushObject(char1);
-      show.save();
+    toggleSideBar() {
+      this.toggleProperty('sideBarOpen');
     }
   }
 });

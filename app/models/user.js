7148810b358d61534798c99fbd72e229843e35d6
Nathan <njgingrich@gmail.com>
2016-07-03 16:02:47 -0400

    began refactoring modals

--- app/models/user.js
+++ app/models/user.js
@@ -5,5 +5,6 @@ import { hasMany } from 'ember-data/relationships';
 export default Model.extend({
   uid: attr('string'),
   name: attr('string'),
+  email: attr('string'),
   shows: hasMany('show')
 });

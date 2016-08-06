09ca3697da318e168626d392f7c73f9b24580573
Nathan <njgingrich@gmail.com>
2016-08-06 11:29:35 -0400

    tried to get async select to show usernames

--- app/components/show-edit.js
+++ app/components/show-edit.js
@@ -3,19 +3,22 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showActions: Ember.inject.service('show-actions'),
   show: null,
-  loadUsernames: () => {
-    console.log(this.get('showActions'));
-    return this.get('showActions').getAllUsers();
-  },
+  loadUsernames: Ember.computed('showActions', function() {
+    return this.get('showActions').getAllUsernames();
+  }),
   userLabelCallback(user) {
     return user.get('name');
   },
 
   actions: {
     addAssignedUser() {
-      this.toggleProperty('addingAssignedUser');
-      let names = this.get('showActions').getAllUsers();
-      console.log(names);
+      this.toggleProperty('addingAssignedUser');/*
+      this.get('showActions').getAllUsers().then((users) => {
+        console.log(users);
+        users.forEach((user) => {
+          console.log(user.get('name'));
+        });
+      });*/
     },
     fileLoaded(file) {
       console.log(file.name, file.type, file.data, file.size);

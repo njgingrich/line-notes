759a7d198d94de0a3eff05b96137e3abd73f6043
nathan <njgingrich@gmail.com>
2016-11-21 14:36:59 -0500

    updated some components to semantic-ui

--- app/components/notes-list.js
+++ app/components/notes-list.js
@@ -11,5 +11,6 @@ export default Ember.Component.extend({
   },
   sortedNotes: null,
   noNotes: Ember.computed.empty('sortedNotes'),
+  classNames: ["ui", "celled", "table"],
   tagName: 'table'
 });

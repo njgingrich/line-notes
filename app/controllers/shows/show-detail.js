f5433d54ff32984c0ef9bcd868dfb8f8da2d13f7
nathan <njgingrich@gmail.com>
2016-12-18 20:24:53 -0500

    installed ember-modal as semantic ui modals werent rendering properly, basic styling for adding note

--- app/controllers/shows/show-detail.js
+++ app/controllers/shows/show-detail.js
@@ -10,42 +10,7 @@ export default Ember.Controller.extend({
 
   actions: {
     selectChar(char) {
-      this.set('activeChar', char);
       window.scrollTo(0,0);
-    },
-    addNote(char, page, line, note, error) {
-      this.get('showActions').addNote(char, page, line, note, error);
-      this.get('notify').success('Added new note!');
-    },
-    editNote(note) {
-      this.get('showActions').editNote(note);
-    },
-    deleteNote(char, note) {
-      this.get('showActions').deleteNote(char, note);
-      this.get('notify').alert('Note deleted!');
-    },
-    deleteAllNotes(char) {
-      this.get('showActions').deleteAllNotes(char);
-    },
-    addChar() {
-      let name = this.get('newCharName');
-      let show = this.get('show');
-      this.get('showActions').addChar(name, show);
-      this.get('notify').success('Added new character!');
-
-      // reset the input
-      this.set('newCharName', '');
-    },
-    deleteChar(char) {
-      let show = this.get('show');
-      this.get('showActions').deleteChar(char, show);
-      this.get('notify').alert('Character deleted!');
-    },
-    editChar(char) {
-      this.toggleProperty('isEditing');
-      if (!this.get('isEditing')) {
-        this.get('showActions').editChar(char);
-      }
     }
   }
 });
--- app/controllers/shows/show-detail/char.js
+++ app/controllers/shows/show-detail/char.js
@@ -2,15 +2,63 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   char: Ember.computed.alias('model'),
+  isShowingCreateModal: false,
 
   actions: {
-    toggleAddNoteModal() {
-      this.toggleProperty('openModal');
+    toggleModal() {
+      this.toggleProperty('isShowingCreateModal');
     },
-    editChar() {
-      console.log("key press submit");
+
+    openModal(name) {
+      console.log('opening modal ' + name);
+      $('.ui.' + name + '.modal').modal('setting', {
+                                   observeChanges: true,
+                                 })
+                                 .modal('show')
+                                 .modal('refresh')
+                                 .modal('refresh');
+    },
+    approveModal(element, component) {
+      alert('approve ' + component.get('name'));
+      return false;
+    },
+    denyModal(element, component) {
+      alert('deny ' + component.get('name'));
+      return true;
+    },
+
+    addNote(char, page, line, note, error) {
+      this.get('showActions').addNote(char, page, line, note, error);
+      this.get('notify').success('Added new note!');
+    },
+    editNote(note) {
+      this.get('showActions').editNote(note);
+    },
+    deleteNote(char, note) {
+      this.get('showActions').deleteNote(char, note);
+      this.get('notify').alert('Note deleted!');
+    },
+    deleteAllNotes(char) {
+      this.get('showActions').deleteAllNotes(char);
+    },
+    addChar() {
+      let name = this.get('newCharName');
+      let show = this.get('show');
+      this.get('showActions').addChar(name, show);
+      this.get('notify').success('Added new character!');
+      // reset the input
+      this.set('newCharName', '');
+    },
+    deleteChar(char) {
+      let show = this.get('show');
+      this.get('showActions').deleteChar(char, show);
+      this.get('notify').alert('Character deleted!');
+    },
+    editChar(char) {
       this.toggleProperty('isEditing');
-      this.attrs.editChar();
+      if (!this.get('isEditing')) {
+        this.get('showActions').editChar(char);
+      }
     }
   }
 });
